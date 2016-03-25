2118893e62f50fee17833317d24d52986633e9fd
Taity-mini <1504693@rgu.ac.uk>
2016-03-25 14:17:43 +0000

    Added Lab 6 & 7

---
+++ Lab6/errors.js
@@ -0,0 +1,15 @@
+function pain () {
+	var a,b
+	var c;
+	
+	a = 10;
+	b = 100;
+	
+	b = + a;
+	c == a * b;
+    document.write(c);
+    document.getElementById("errors");
+	console.log(a);
+	console.log(b);
+	console.log(c);
+}
